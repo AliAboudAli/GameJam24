e72ba5e4c389d7be2578f662d44002da59038926
roelsiemens <roelsiemens@gmail.com>
2024-02-13 09:20:51 +0100

    test

---
+++ Assets/scripts/menu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public void quitgame()
+    {
+        Application.Quit();
+    }
+
+    public void playgame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Continue()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
