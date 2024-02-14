db7e058f654131ca932f85e292490f571c9fec89
ALi <aaali2@st.noorderpoort.nl>
2024-02-14 10:28:21 +0100

    UI

--- Assets/Ali/Scripts/Switch.cs
+++ Assets/Ali/Scripts/Switch.cs
@@ -1,55 +1,98 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class Switch : MonoBehaviour
 {
     public GameObject[] Courasel;
-    public int index;
+    public GameObject[] charactersPrefabs;
+    private int index;
+    public TextMeshProUGUI[] Names;
+    public GameObject SpawnedObject;
     
+
     // Start is called before the first frame update
     void Start()
     {
         index = 0;
+        ShowCurrent();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (index >= 4)
-            index = 4;
-
-        if (index < 0) ;
-        index = 0;
-
-        if (index == 0)
-        {
-            Courasel[0].gameObject.SetActive(true);
-        }
-    }
-
+    
     public void Next()
     {
-        index += 1;
+        index++;
+        if (index >= Courasel.Length)
+            index = 0;
 
-        for (int i = 0; i < Courasel.Length; i++)
-        {
-            Courasel[i].gameObject.SetActive(false);
-            Courasel[index].gameObject.SetActive(true);
-        }
-        Debug.Log(index);
+        ShowCurrent();
     }
 
     public void Previous()
     {
-        index -= 1;
+        index--;
+        if (index < 0)
+            index = Courasel.Length - 1;
 
+        ShowCurrent();
+    }
+
+    private void ShowCurrent()
+    {
+        // Activate only the GameObject at the current index
         for (int i = 0; i < Courasel.Length; i++)
         {
-            Courasel[i].gameObject.SetActive(false);
-            Courasel[index].gameObject.SetActive(true);
+            Courasel[i].SetActive(i == index);
         }
-        Debug.Log(index);
-    } 
+
+        // Update the names in the TextMeshProUGUI components
+        for (int i = 0; i < Names.Length; i++)
+        {
+            if (i == index)
+            {
+                Names[i].gameObject.SetActive(true);
+                Names[i].text = GetElementName(index);
+            }
+            else
+            {
+                Names[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private string GetElementName(int index)
+    {
+        // Provide the names for each element here based on the index
+        switch (index)
+        {
+            case 0:
+                return "NanN1";
+            case 1:
+                return "NanN2";
+            case 2:
+                return "NanN3";
+            case 3:
+                return "NanN4";
+            default:
+                return "";
+        }
+    }
+
+    public void SpawnSelectedCharacter()
+    {
+        //check if the index is valid 
+        if (index >= 0 && index < charactersPrefabs.Length)
+        {
+            //Destroy the previously spawned character prefab
+            if (SpawnedObject != null)
+            {
+                Destroy(SpawnedObject);
+            }
+            
+            //Instantiate the selected character prefab
+            SpawnedObject = Instantiate(charactersPrefabs[index], transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Debug.Log("Invalid index");
+        }
+    }
 }
