3f58cfab28457a8a43fb3f8d4fc4bd9e5f760e94
max <maxquirijns@gmail.com>
2024-02-14 14:10:20 +0100

    shit gefixt en nieuwe animations

--- Assets/Max/Scripts/Movement.cs
+++ Assets/Max/Scripts/Movement.cs
@@ -9,7 +9,11 @@ public class Movement : MonoBehaviour
     CharacterController controller; 
 
     public Animator animator;
+    AnimatorStateInfo animStateInfo;
+    public float NTime;
+    bool animationFinished;
 
+    public bool IsAttacking = false;
     void Start()
     {
         controller = GetComponent<CharacterController>(); 
@@ -21,29 +25,73 @@ public class Movement : MonoBehaviour
 
         Debug.Log("Vertical Input: " + verticalInput);
 
+
+   
+
         Vector3 moveDirection = new Vector3(0f, 0f, verticalInput).normalized;
 
-        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
 
-        if (verticalInput > 0)
+        if (!IsAttacking)
+        {
+            controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+        }
+
+        if (verticalInput == 1 && animationFinished == true && !IsAttacking)
         {
             animator.Play("WalkForward");
         }
 
-        if (verticalInput < 0)
+        if (verticalInput == -1 && !IsAttacking)
         {
             animator.Play("WalkBackward");
         }
 
-
         if (Input.GetButtonDown("Fire1"))
         {
+            IsAttacking = true;
             animator.Play("Punch");
-        }
-        if (!this.animator.GetCurrentAnimatorStateInfo(0).IsName("Punch") && verticalInput == 0)
-        {
-            animator.Play("Idle");
+
+            
+
+       
         }
 
+        animStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        NTime = animStateInfo.normalizedTime;
+
+        if (NTime > 0.88f)
+        {
+            print("animation complete");
+            animationFinished = true;
+            IsAttacking = false;
+        }
+
+        /*if (!this.animator.GetCurrentAnimatorStateInfo(0).IsName("Punch") && verticalInput == 0)
+        {
+            animator.Play("Idle");
+        }*/
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            IsAttacking = true;
+            animator.Play("Mma Kick");
+        }
+
+        /*if (!this.animator.GetCurrentAnimatorStateInfo(0).IsName("Mma Kick") && verticalInput == 0)
+        {
+            animator.Play("Idle");
+        }*/
+
+        if (Input.GetButtonDown("Fire3"))
+        {
+            IsAttacking = true;
+            animator.Play("Flying Bicycle Kick");
+        }
+
+        /*if (!this.animator.GetCurrentAnimatorStateInfo(0).IsName("Hurricane Kick") && verticalInput == 0)
+        {
+            animator.Play("Idle");
+        }*/
+        
     }
 }
